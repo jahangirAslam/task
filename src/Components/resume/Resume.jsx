437711e85dc3ffc572d01925afa03a893b644a1f
jahangirAslam <hafiz.j.aslam@gmail.com>
2023-05-30 11:28:27 +0500

    pdf selectable

--- src/Components/resume/Resume.jsx
+++ src/Components/resume/Resume.jsx
@@ -1,26 +1,37 @@
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
 import React from "react";
-import "./resume.css";
+import { ArrowDown } from 'react-bootstrap-icons';
+import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import html2pdf from 'html2pdf.js';
-import { ArrowDown } from 'react-bootstrap-icons';
+import "./resume.css";
+
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 const Resume = () => {
     const handleDownload = () => {
         const element = document.getElementById('resume');
-        const opt = {
-            margin: 0.5,
-            filename: 'resume.pdf',
-            image: { type: 'jpeg', quality: 0.98 },
-            html2canvas: { scale: 2 },
-            jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+        const content = [
+            {
+                text: element.innerText,
+                fontSize: 12,
+                margin: [0, 0, 0, 12], // Optional margin
+            },
+        ];
+
+        // Define the PDF document
+        const docDefinition = {
+            content,
         };
-        html2pdf().set(opt).from(element).save();
+
+        // Generate and save the PDF
+        pdfMake.createPdf(docDefinition).download('resume.pdf');
     };
 
     return (
         <div>
-            <Container fluid className="resume" id="resume" style={{padding:"20px"}} >
+            <Container fluid className="resume" id="resume" style={{ padding: "20px" }} >
                 <Row className="resume-bg">
                     <Col sm={6} className="resume-address">
                         <h3>Nabeel Raza</h3>
@@ -42,23 +53,24 @@ const Resume = () => {
                     <Col sm={12} className="resume-experience">
                         <p>Education</p>
                     </Col>
-                    <Col sm={5}  className="resume-study-Mas">
+                    <Col sm={5} className="resume-study-Mas">
                         <h3>Master in Data Science</h3>
                         <p>ITU Lahore <br /> September 2018 - July 2020<br /></p>
                     </Col>
-                    <Col sm={3}  className="resume-study-bach offset-sm-4">
+                    <Col sm={3} className="resume-study-bach offset-sm-4">
                         <h3>BSc Electrical Engineering</h3>
                         <p>UET Lahore <br /> September 2014 - May 2018<br /></p>
                     </Col>
-                    <Col sm={12}>
-                        <div className="downloadBtn">
-                            <button onClick={handleDownload}>
-                            <ArrowDown size={24} />     Download Resume
-                            </button>
-                        </div>
-                    </Col>
+
                 </Row>
             </Container>
+            <Col sm={12}>
+                <div className="downloadBtn">
+                    <button onClick={handleDownload}>
+                        <ArrowDown size={24} />     Download Resume
+                    </button>
+                </div>
+            </Col>
         </div>
     );
 }
